c495edf7cdd0ee60e59cf725d14e936657ee326c
unknown <shashi902794@gmail.com>
2024-01-08 22:46:18 +0530

    user signup flow

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import mongoose from 'mongoose'
+import userRoutes from './routes/user'
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+app.use("/api/users", userRoutes);
+app.listen(7000, ()=>{
+    console.log("server running on localhost:7000")
+})
