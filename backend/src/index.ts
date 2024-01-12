bbb7f990707336516326efe59a4376e7ca9e3671
unknown <shashi902794@gmail.com>
2024-01-12 22:28:21 +0530

    initial layout

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import "dotenv/config";
 import mongoose from 'mongoose'
 import userRoutes from './routes/user'
 import authRoutes from './routes/auth'
+import cookieParser from "cookie-parser";
+import path from 'path';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
@@ -13,12 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({origin: process.env.FRONTEND_URL,
 credentials:true}));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/users", userRoutes);
-app.use("api/auth", authRoutes)
+app.use("/api/auth", authRoutes)
 app.listen(7000, ()=>{
     console.log("server running on localhost:7000")
 })
-function cookieParser(): any {
-    throw new Error('Function not implemented.');
-}
-
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -31,13 +31,19 @@ router.post("/login", [check("email", "Email is required").isEmail(), check("pas
         res.cookie("auth_token", token, {httpOnly:true, secure: process.env.NODE_ENV === "production"});
         res.status(200).json({userId: user._id});
     } catch(error){
-        console.log(error);
         res.status(500).json({message: "Something went wrong"})
     }
 })
 
-router.get("/validate-token", verifyToken,(req:Request, res:Response)=>{
-res.status(200).send({userId: req.userId})
+router.post("/logout", (req:Request, res:Response)=>{
+   res.cookie("auth_token", "",{
+    expires: new Date(0)
+   })
+   res.send();
 })
+
+router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+    res.status(200).send({ userId: req.userId });
+  });
 
 export default router
