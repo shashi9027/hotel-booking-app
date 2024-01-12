f7d16f914657558c8ecf6c558eb04023a429fc3c
unknown <shashi902794@gmail.com>
2024-01-12 07:50:35 +0530

    login signup flow

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,22 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose'
 import userRoutes from './routes/user'
+import authRoutes from './routes/auth'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors());
+app.use(cors({origin: process.env.FRONTEND_URL,
+credentials:true}));
 app.use("/api/users", userRoutes);
+app.use("api/auth", authRoutes)
 app.listen(7000, ()=>{
     console.log("server running on localhost:7000")
 })
+function cookieParser(): any {
+    throw new Error('Function not implemented.');
+}
+
