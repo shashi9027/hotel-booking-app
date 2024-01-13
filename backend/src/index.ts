c0536ff5947973e9f453e65724edc7b3650bfc73
unknown <shashi902794@gmail.com>
2024-01-13 12:18:40 +0530

    cors issue fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,12 @@ import path from 'path';
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://hotel-booking-app-xuxs.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
