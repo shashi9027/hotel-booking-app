45535ad4a71d3985c89dd94621fec041c141782f
unknown <shashi902794@gmail.com>
2024-01-13 13:02:49 +0530

    issue fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,13 +10,6 @@ import path from 'path';
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app = express();
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://hotel-booking-app-xuxs.onrender.com');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials', "true");
-    next();
-  });
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
