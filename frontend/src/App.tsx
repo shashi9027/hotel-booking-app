a117fa93a59245b48c903f61bfd070167e9d42d3
unknown <shashi902794@gmail.com>
2024-01-14 21:36:55 +0530

    add hotel functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,11 @@ import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom
 import Layout from './layouts/Layout'
 import Register from './pages/Register'
 import SignIn from './pages/SignIn'
+import { useAppContext } from './contexts/AppContext';
+import AddHotel from './pages/AddHotel';
 
 function App() {
- 
+  const { isLoggedIn } = useAppContext();
   return (
     <Router>
          <Routes>
@@ -17,6 +19,51 @@ function App() {
                 <p>Home Page</p>
                 </Layout>} />
               <Route path="/register" element={<Layout><Register/></Layout>}/>
+              {isLoggedIn && (
+          <>
+            {/* <Route
+              path="/hotel/:hotelId/booking"
+              element={
+                <Layout>
+                  <Booking />
+                </Layout>
+              }
+            /> */}
+
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+            {/* <Route
+              path="/edit-hotel/:hotelId"
+              element={
+                <Layout>
+                  <EditHotel />
+                </Layout>
+              }
+            /> */}
+            {/* <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            /> */}
+            {/* <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
+                </Layout>
+              }
+            /> */}
+          </>
+        )}
               <Route path="*" element={<Navigate to="/" />} />
               <Route path="/sign-in" element={
                 <Layout>
