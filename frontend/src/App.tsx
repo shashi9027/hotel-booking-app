9fec2e9b3d3aa042a872bd42e5b9632a24a6f709
unknown <shashi902794@gmail.com>
2024-01-15 19:41:26 +0530

    edit hotel feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Register from './pages/Register'
 import SignIn from './pages/SignIn'
 import { useAppContext } from './contexts/AppContext';
 import AddHotel from './pages/AddHotel';
+import MyHotels from './pages/MyHotels';
+import EditHotel from './pages/EditHotels';
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -38,22 +40,22 @@ function App() {
                 </Layout>
               }
             />
-            {/* <Route
+            <Route
               path="/edit-hotel/:hotelId"
               element={
                 <Layout>
                   <EditHotel />
                 </Layout>
               }
-            /> */}
-            {/* <Route
+            />
+            <Route
               path="/my-hotels"
               element={
                 <Layout>
                   <MyHotels />
                 </Layout>
               }
-            /> */}
+            />
             {/* <Route
               path="/my-bookings"
               element={
